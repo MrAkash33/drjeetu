8a3636b1d0e0074561ae37bdd1e62288fba033dd
Mr Akash <79826594+MrAkash33@users.noreply.github.com>
2024-06-14 12:47:20 +0530

    Update HeroBanner.jsx

--- components/HeroBanner/HeroBanner.jsx
+++ components/HeroBanner/HeroBanner.jsx
@@ -9,11 +9,10 @@ const HeroBanner = () => {
             <div className="col-lg-6 col-md-12 col-xl-7">
               <div className="block">
                 <div className="divider mb-3"></div>
-                <span className="text-uppercase text-sm letter-spacing ">Professional Care For Your Health</span>
-                <h1 className="mb-3 mt-3">Good Health Moves
-                  Us Forward</h1>
+                <span className="text-uppercase text-sm letter-spacing ">Expert Care for Your Well-Being</span>
+                <h1 className="mb-3 mt-3">A healthy outside starts from the inside.</h1>
 
-                <p className="mb-4 pr-5">Humanity stands as a cornerstone of professionalism for any doctor. Here at our clinic, we prioritize your holistic well-being, nurturing both your mental and physical health with meticulous care.</p>
+                <p className="mb-4 pr-5">At our clinic, we believe that compassion is the bedrock of medical professionalism. We are dedicated to your holistic well-being, carefully tending to both your mental and physical health with the utmost attention and care.</p>
                 <div className="btn-container ">
                   {/* <a href="appoinment.html" target="_blank" className="btn btn-main-2 btn-icon btn-round-full">Make appoinment
                     <i className="icofont icofont-simple-right ml-2  "></i></a> */}
