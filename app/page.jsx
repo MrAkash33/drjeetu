a5259c258c68a5317c18fa9f0a52717a06eaa52f
Mr Akash <79826594+MrAkash33@users.noreply.github.com>
2024-06-14 12:58:42 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -17,7 +17,7 @@ export default function Home() {
       <Counter />
       <Services />
 {/*       <Appoinment /> */}
-      <Testimonial />
+{/*       <Testimonial /> */}
       <Clients />
     </main>
   );
